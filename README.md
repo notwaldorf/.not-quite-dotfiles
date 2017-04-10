3cb70bf661180da04947bb77ada476ca0d22c15d
Monica Dinculescu <noms@google.com>
2017-04-10 16:03:01 -0700

    reorganize a bit

--- README.md
+++ README.md
@@ -14,6 +14,5 @@ Looks like this:
 Add this to your `~\.zshrc` file. I use oh-my-zsh, and all of this works for that. If you don't, everything might break and this might open the Hellmouth.
 
 <pre>
-source $HOME/.not-quite-dotfiles/aliases
-source $HOME/.not-quite-dotfiles/prompt
+source $HOME/.not-quite-dotfiles/custom.zsh
 </pre>
---
+++ custom.zsh
@@ -0,0 +1,38 @@
+ZSH_THEME="no-theme"
+ENABLE_CORRECTION="true"
+DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+local LC_ALL=en_US.UTF-8
+
+source $ZSH/oh-my-zsh.sh
+source $HOME/.not-quite-dotfiles/prompt
+source $HOME/.not-quite-dotfiles/aliases
+source $HOME/.not-quite-dotfiles/chrome
+
+export PATH="/usr/local/git/current/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
+
+export NVM_DIR="/Users/noms/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+care_bot() {
+  node ~/Code/tiny-terminal-care/index.js
+}
+
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+echo ""
+echo "$Red(●っゝω・)っ ～☆～☆ $Color_Off $(care_bot) $Red ☆～☆～ $Color_Off"
+echo ""
