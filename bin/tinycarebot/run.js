66f8c20760ae1c27de8e88a1312c0973879f63b3
Monica Dinculescu <noms@google.com>
2017-04-10 17:20:53 -0700

    add tinycarebot script

---
+++ bin/tinycarebot/run.js
@@ -0,0 +1,29 @@
+var Twit = require('twit')
+//var fs = require('fs');
+var env = require('node-env-file');
+
+/* These values come from a .env file of the form:
+CONSUMER_KEY='...'
+CONSUMER_SECRET='...'
+ACCESS_TOKEN='...'
+ACCESS_TOKEN_SECRET='...'
+*/
+env(__dirname + '/.env');
+
+var T = new Twit({
+  consumer_key:process.env.CONSUMER_KEY,
+  consumer_secret:process.env.CONSUMER_SECRET,
+  access_token:process.env.ACCESS_TOKEN,
+  access_token_secret:process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+});
+
+var options = { screen_name: 'tinycarebot', exclude_replies:true, count: 1 };
+
+T.get('statuses/user_timeline', options , function(err, data) {
+  for (var i = 0; i < data.length ; i++) {
+    var output = data[i].text + "\n";
+    //fs.writeFile("./care.txt", output);
+    console.log(output);
+  }
+});
