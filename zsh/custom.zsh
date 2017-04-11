0b78b54c4ff0ad08088932d504c96984798946bb
Monica Dinculescu <noms@google.com>
2017-04-10 17:42:10 -0700

    move things to a zsh folder

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -7,9 +7,9 @@ export LC_ALL=en_US.UTF-8
 local LC_ALL=en_US.UTF-8
 
 source $ZSH/oh-my-zsh.sh
-source $HOME/.not-quite-dotfiles/prompt
-source $HOME/.not-quite-dotfiles/aliases
-source $HOME/.not-quite-dotfiles/chrome
+source $HOME/.not-quite-dotfiles/zsh/prompt
+source $HOME/.not-quite-dotfiles/zsh/aliases
+source $HOME/.not-quite-dotfiles/zsh/chrome
 
 export PATH="/usr/local/git/current/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
 
@@ -17,7 +17,7 @@ export NVM_DIR="/Users/noms/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 care_bot() {
-  node ~/Code/tiny-terminal-care/index.js
+  node ~/.not-quite-dotfiles/bin/tinycarebot/run.js
 }
 
 # Reset
@@ -34,5 +34,5 @@ Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'        # White
 
 echo ""
-echo "$Red(●っゝω・)っ ～☆～☆ $Color_Off $(care_bot) $Red ☆～☆～ $Color_Off"
+echo "$Green(●っゝω・)っ ～☆～☆ $Color_Off $(care_bot) $Green ☆～☆～ $Color_Off"
 echo ""
